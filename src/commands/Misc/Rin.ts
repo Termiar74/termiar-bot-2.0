44564fba339167c1238592c83bfabca5a66209ba
yashcybrot <79310401+yashcybrot@users.noreply.github.com>
2022-05-03 16:09:19 +0530

    Update Rin.ts

--- src/commands/Misc/Rin.ts
+++ src/commands/Misc/Rin.ts
@@ -8,7 +8,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'rin',
+            command: 'level',
             description: 'Displays the info.',
             category: 'misc',
             usage: `${client.config.prefix}rin`
@@ -22,7 +22,7 @@ export default class Command extends BaseCommand {
         let rin = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: rin }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `🎗 Rin\n\n⚜ *Description: Maintained fork of void* \n\n ⭐*URL-https://github.com/Oreki-san/RIN* \n` }
+            caption: `👉🏻  𝐋𝐄𝐕𝐄𝐋 𝐁𝐎𝐓\n\n⚜ *𝐃𝐄𝐒𝐂𝐑𝐈𝐏𝐓𝐈𝐎𝐍: 𝐇𝐀𝐂𝐊𝐓𝐈 𝐋𝐄𝐕𝐄𝐋 𝐁𝐎𝐓 𝐂𝐑𝐄𝐀𝐓𝐄𝐃 𝐁𝐘 𝐌𝐑. 𝐇𝐀𝐂𝐊𝐓𝐈𝐕𝐈𝐒𝐓. 𝐓𝐇𝐈𝐒 𝐁𝐎𝐓 𝐇𝐀𝐕𝐄 𝐑𝐈𝐂𝐇 𝐀𝐍𝐈𝐌𝐄 𝐀𝐍𝐃 𝐍𝐒𝐅𝐖 𝐅𝐄𝐀𝐓𝐔𝐑𝐄* \n\n ⭐*𝐔𝐑𝐋-𝐓𝐇𝐈𝐒 𝐁𝐎𝐓 𝐈𝐒 𝐏𝐑𝐈𝐕𝐀𝐓𝐄* \n` }
         )
     }
 }
